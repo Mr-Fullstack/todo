c75770ff8320a6bac10e583394403caeca4b1bee
Marcos Antonio Cerqueira Souza de Freitas <marcosfullstack@gmail.com>
2021-07-21 09:23:32 -0300

    desafio completado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,42 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+
+    let task = {
+       id : new Date().getTime(),
+       title:newTaskTitle,
+       done:false
+    }
+
+    setTasks(oldstate => [...oldstate,task])
+
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
-  }
+    const updatedTasks = tasks.map(task => ({...task}))
+
+    updatedTasks.find(taskItem => {
+      if(taskItem.id == id){
+        taskItem.done = !taskItem.done
+      }
+    })
+      
+    setTasks(updatedTasks)
+    
+}
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldstate => oldstate.filter(
+      task => task.id != id
+   ))
+
   }
 
   return (
